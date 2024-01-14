d71b556ea0d0ba72dfaf6ec0f79b8c8c82d35e57
Pedro Palma <pedrolucaspalma@gmail.com>
2024-01-14 16:56:28 -0300

    working .env config

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,16 @@
-
 import express from "express"
 import router from "./routes"
+
+require('dotenv').config({ path: process.env.NODE_ENV === 'test' ? '.env.test' : '.env' })
+
 const app = express()
 const port = process.env.PORT
 
 app.use(router)
 
 app.listen(port, () => {
-	console.log(`Example app listening on port ${port}`)
+	console.log(`mwro API listening on port ${port}`)
+
 })
 
 process.on("SIGINT", () => {
