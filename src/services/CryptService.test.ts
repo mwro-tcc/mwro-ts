382974ba762851f56e6d97401564eb709a888fb2
Pedro Palma <pedrolucaspalma@gmail.com>
2024-01-13 21:57:44 -0300

    add more tests

--- src/services/CryptService.test.ts
+++ src/services/CryptService.test.ts
@@ -4,7 +4,7 @@ import {CryptService} from './CryptService';
 const cryptService = new CryptService();
 
 
-describe("Encrypting Service", () => {
+describe.concurrent("Encrypting Service", () => {
     test("hashPassword should return a object containing the hashed password and the salt generated", async () => {
         const testingPassword = "testingPassword";
         
@@ -13,5 +13,38 @@ describe("Encrypting Service", () => {
         expect(hashedPasswordData.password).not.toBe(testingPassword);
         expect(hashedPasswordData.salt).toBeTypeOf("string");
         expect(hashedPasswordData.password).toBeTypeOf("string");
+    });
+
+    test("comparePasswords should return true if the password is correct", async () => {
+        const testingPassword = "testingPassword";
+        const hashedPasswordData = await cryptService.hashPassword(testingPassword);
+
+        const result = await cryptService.comparePasswords(hashedPasswordData, testingPassword);
+
+        expect(result).toBe(true);
+    });
+
+    test("comparePasswords should return false if the password is incorrect", async () => {
+        const testingPassword = "testingPassword";
+        const hashedPasswordData = await cryptService.hashPassword(testingPassword);
+
+        const result = await cryptService.comparePasswords(hashedPasswordData, "incorrectPassword");
+
+        expect(result).toBe(false);
+    });
+
+    test("generatePasswordToken should return a token", () => {
+        const token = cryptService.generatePasswordToken("userId");
+
+        expect(token).toBeTypeOf("string");
+    });
+
+    test("verifyPasswordToken should return the userId and passwordToken", () => {
+        const token = cryptService.generatePasswordToken("userId");
+
+        const result = cryptService.verifyPasswordToken(token);
+
+        expect(result.userId).toBe("userId");
+        expect(result.passwordToken).toBeTypeOf("string");
     });
 });
