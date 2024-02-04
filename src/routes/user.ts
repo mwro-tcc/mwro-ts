0f126fc6f662c46f2a89a335472bf93e756a5e5b
Pedro Palma <pedrolucaspalma@gmail.com>
2024-02-04 15:41:04 -0300

    add new route

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,5 +4,6 @@ import { userController } from "../controllers/UserController";
 const router = express.Router();
 
 router.post('/sign-up', userController.signUp())
+router.post('/sign-in', userController.signIn())
 
 export default router
