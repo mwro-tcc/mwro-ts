3363a601ad2d29c1dbea5df8e019f178839c3661
Pedro Palma <pedrolucaspalma@gmail.com>
2024-02-25 07:45:45 -0300

    Make vite tests run sequentially

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,11 @@ import { testEnvValues } from './src/constants/EnvironmentVariables';
 
 export default defineConfig({
 	test: {
+		poolOptions: {
+			threads: {
+				singleThread: true
+			},
+		},
 		env: testEnvValues
 	},
 });
