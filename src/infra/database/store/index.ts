2bcf81e29b90b49fb217de95454fdf68a0650bc8
Pedro Palma <pedrolucaspalma@gmail.com>
2024-03-11 18:32:32 -0300

    Add new store adapter method

--- src/infra/database/store/index.ts
+++ src/infra/database/store/index.ts
@@ -1,6 +1,7 @@
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { IStoreAdapter } from "./interface";
 import { NewStore, Store, stores } from "../../../database/schema/stores";
+import { eq } from "drizzle-orm";
 
 class StoreAdapter implements IStoreAdapter {
     constructor(private readonly db: NodePgDatabase) {}
@@ -12,6 +13,11 @@ class StoreAdapter implements IStoreAdapter {
 
     async bulkCreate(payload: NewStore[]): Promise<void> {
         await this.db.insert(stores).values(payload).returning();
+    }
+
+    async findByUuid(uuid: string): Promise<Store> {
+        const data = await this.db.select().from(stores).where(eq(stores.uuid, uuid));
+        return data[0];
     }
 }
 
--- src/infra/database/store/interface.ts
+++ src/infra/database/store/interface.ts
@@ -3,4 +3,5 @@ import { NewStore, Store } from "../../../database/schema/stores";
 export interface IStoreAdapter {
     create(data: NewStore): Promise<Store>;
     bulkCreate(payload: NewStore[]): Promise<void>;
+    findByUuid(uuid: string): Promise<Store>;
 }
