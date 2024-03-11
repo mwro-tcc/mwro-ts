926dde1743f7106f42cfc72b9d85341fda9d0284
Pedro Palma <pedrolucaspalma@gmail.com>
2024-03-11 18:33:30 -0300

    Add product adapter

--- src/infra/database/product/index.ts
+++ src/infra/database/product/index.ts
@@ -1,36 +1,57 @@
 import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { IProductAdapter } from "./interface";
-import { NewProduct, Product } from "../../../database/schema/products";
+import { NewProduct, Product, products } from "../../../database/schema/products";
 
-class ProductAdapter implements IProductAdapter{
-    constructor(
-        private readonly db: NodePgDatabase
-    ){}
-    async bulkCreate(payload:NewProduct[]): Promise<void> {
-        throw new Error('')
+class ProductAdapter implements IProductAdapter {
+    constructor(private readonly db: NodePgDatabase) {}
+    async bulkCreate(payload: NewProduct[]): Promise<void> {
+        throw new Error("");
     }
 
     async create(params: NewProduct): Promise<Product> {
-        throw new Error('')
+        const data = await this.db.insert(products).values(params).returning();
+        return data[0];
     }
 
     async delete(uuid: string): Promise<void> {
-        throw new Error('')
+        throw new Error("");
     }
 
     async findByUuid(uuid: string): Promise<Product> {
-        throw new Error('')
+        throw new Error("");
     }
 
-    async listCreatedByUserUuid(userUuid: string, params: { limit: number; offset: number; }): Promise<any> {
-        throw new Error('')
+    async listCreatedByUserUuid(
+        userUuid: string,
+        params: { limit: number; offset: number },
+    ): Promise<any> {
+        throw new Error("");
     }
-    
-    async update(uuid: string, data: Partial<{ name: string; price: number; stock: number; storeUuid: string; uuid?: string | undefined; description?: string | null | undefined; createdAt?: string | undefined; }>): Promise<{ uuid: string; name: string; price: number; stock: number; description: string | null; storeUuid: string; createdAt: string; }> {
-        throw new Error('')
+
+    async update(
+        uuid: string,
+        data: Partial<{
+            name: string;
+            price: number;
+            stock: number;
+            storeUuid: string;
+            uuid?: string | undefined;
+            description?: string | null | undefined;
+            createdAt?: string | undefined;
+        }>,
+    ): Promise<{
+        uuid: string;
+        name: string;
+        price: number;
+        stock: number;
+        description: string | null;
+        storeUuid: string;
+        createdAt: string;
+    }> {
+        throw new Error("");
     }
 }
 
-export function makeProductAdapter(db: NodePgDatabase){
-    return new ProductAdapter(db)
+export function makeProductAdapter(db: NodePgDatabase) {
+    return new ProductAdapter(db);
 }
