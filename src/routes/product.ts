6334f5d54e06464ee1d5db6fb0d40c26c27beb5f
Pedro Palma <pedrolucaspalma@gmail.com>
2024-07-07 12:37:32 -0300

    upd missing route name

--- src/routes/product.ts
+++ src/routes/product.ts
@@ -4,7 +4,7 @@ import { productController } from "../controllers/ProductController";
 
 const router = express.Router();
 
-router.post("/create", authenticationMiddleware(), productController.create());
+router.post("/", authenticationMiddleware(), productController.create());
 router.get("/:uuid", authenticationMiddleware(), productController.findByUuid());
 router.put("/:uuid", authenticationMiddleware(), productController.update());
 router.delete("/:uuid", authenticationMiddleware(), productController.delete());
