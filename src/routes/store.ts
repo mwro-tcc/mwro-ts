83404f38abbd8a9355a475259f10a012760b71b4
Pedro Palma <pedrolucaspalma@gmail.com>
2024-07-21 19:46:23 -0300

    fix wrong verb on route

--- src/routes/store.ts
+++ src/routes/store.ts
@@ -5,7 +5,7 @@ import { storeController } from "../controllers/StoreController";
 const router = express.Router();
 
 router.post("/", authenticationMiddleware(), storeController.create());
-router.post("/mine", authenticationMiddleware(), storeController.listMyStores());
+router.get("/mine", authenticationMiddleware(), storeController.listMyStores());
 router.put("/:uuid", authenticationMiddleware(), storeController.update());
 router.get("/:uuid", authenticationMiddleware(), storeController.findByUuid());
 router.delete("/:uuid", authenticationMiddleware(), storeController.delete());
