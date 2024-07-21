917de77a2ebb50e076b9bd86c5aebf852467162b
Pedro Palma <pedrolucaspalma@gmail.com>
2024-07-21 16:10:06 -0300

    Add new possible error message

--- src/constants/StatusError.ts
+++ src/constants/StatusError.ts
@@ -17,6 +17,7 @@ export const ErrorMessages = {
     assetNotFound: "Asset not found",
     userNotAnAdmin: "User is not an admin of this community",
     userNotCreator: "User is not the creator of this community",
+    userNotCreatorStore: "User is not the creator of this store",
     storeNotFound: "Store not found",
     userIsNotStoreOwner: "Only the owner of the store may perform this action",
 };
