970fd430d6979c505e21a8e0336a3854c2803e00
Pedro Palma <pedrolucaspalma@gmail.com>
2024-07-21 13:58:28 -0300

    make communityUuid optional in create store form

--- src/validations/CreateStore.ts
+++ src/validations/CreateStore.ts
@@ -2,7 +2,7 @@ import { z } from "zod";
 
 export const createStoreSchema = z.object({
     body: z.object({
-        communityUuid: z.string().trim().uuid(),
+        communityUuid: z.string().trim().uuid().optional(),
         name: z.string().trim(),
         description: z.string().optional(),
     }),
