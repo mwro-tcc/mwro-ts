98cfe34e8e701b032f01b3a0bf6b1c61e8fa8c57
Pedro Palma <pedrolucaspalma@gmail.com>
2024-10-09 22:31:17 -0300

    Add new "images" table and model

---
+++ src/database/schema/images.ts
@@ -0,0 +1,22 @@
+import { sql } from "drizzle-orm";
+import { customType, date, pgTable, uuid, varchar } from "drizzle-orm/pg-core";
+const bytea = customType<{ data: Buffer; notNull: false; default: false }>({
+    dataType() {
+        return "bytea"
+    },
+})
+
+export const images = pgTable("images", {
+    uuid: uuid("uuid")
+        .primaryKey()
+        .default(sql`gen_random_uuid()`),
+
+    assetType: varchar("assetType").notNull(),
+    assetUuid: uuid("assetUuid").notNull(),
+
+    content: bytea("content").notNull(),
+    createdAt: date("createdAt").defaultNow().notNull(),
+});
+
+export type Image = typeof images.$inferSelect;
+export type NewImage = typeof images.$inferInsert;
