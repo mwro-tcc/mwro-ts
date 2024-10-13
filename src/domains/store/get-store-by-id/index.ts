22e1a0571b26cd966711e4f6410f748bfae18e84
Pedro Palma <pedrolucaspalma@gmail.com>
2024-10-13 13:41:26 -0300

    Add owner to Get store by id usecase

--- src/domains/store/get-store-by-id/index.ts
+++ src/domains/store/get-store-by-id/index.ts
@@ -5,30 +5,35 @@ import { IStoreAdapter } from "../../../infra/database/store/interface";
 import { databaseConnectionPool } from "../../../database";
 import { ErrorMessages, StatusError } from "../../../constants/StatusError";
 import { IProductAdapter } from "../../../infra/database/product/interface";
-import { makePgProductAdapter } from "../../../infra/database/product";
 import { Community } from "../../../database/schema/communities";
 import { ICommunityAdapter } from "../../../infra/database/community/interface";
 import { makePgCommunityAdapter } from "../../../infra/database/community";
+import { User } from "../../../database/schema/users";
+import { IUserAdapter } from "../../../infra/database/user/interface";
+import { makePgUserAdapter } from "../../../infra/database/user";
 
 class GetStoreById {
     constructor(
         private readonly storeAdapter: IStoreAdapter,
         private readonly communityAdapter: ICommunityAdapter,
-    ) {}
+        private readonly userAdapter: IUserAdapter
+    ) { }
 
     async execute(storeUuid: string): Promise<StoreWithLazyLoadedAssets> {
         const baseStore = await this.storeAdapter.findByUuid(storeUuid);
 
+
+        let community: Community | null = null;
+        if (baseStore.communityUuid) {
+            community = await this.communityAdapter.findByUuid(baseStore.communityUuid);
+        }
+
+        const owner = await this.userAdapter.findByUuid(baseStore.userUuid)
         const store: StoreWithLazyLoadedAssets = {
             ...baseStore,
-            community: null,
+            community,
+            owner
         };
-
-        if (baseStore.communityUuid) {
-            const community = await this.communityAdapter.findByUuid(baseStore.communityUuid);
-
-            store.community = community;
-        }
 
         return store;
     }
@@ -36,8 +41,9 @@ class GetStoreById {
 
 type StoreWithLazyLoadedAssets = Store & {
     community: Community | null;
+    owner: User;
 };
 
 export function makeGetStoreByIdUseCase(db: NodePgDatabase = databaseConnectionPool) {
-    return new GetStoreById(makePgStoreAdapter(db), makePgCommunityAdapter(db));
+    return new GetStoreById(makePgStoreAdapter(db), makePgCommunityAdapter(db), makePgUserAdapter(db));
 }
