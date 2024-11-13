c19e2ebe64f8102c80281f54a81b2afd6dbb1467
Pedro Palma <pedrolucaspalma@gmail.com>
2024-11-13 08:45:38 -0300

    Upd loggingMiddleware

--- src/middlewares/logger/index.ts
+++ src/middlewares/logger/index.ts
@@ -2,12 +2,12 @@ import { Request, NextFunction, Response } from "express";
 import { logger } from "../../services/logger/logger";
 
 export function loggingMiddleware() {
-    return function (req: Request, _: Response, next: NextFunction) {
+    return function(req: Request, _: Response, next: NextFunction) {
         try {
             logger.info({
                 info: "Received request:",
                 method: req.method,
-                url: req.url,
+                url: req.originalUrl,
             });
         } catch (e) {
             console.log("internal logger error:", e);
