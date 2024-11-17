8e187d52b5665d4f459739b7c822fb0005b89fff
Pedro Palma <pedrolucaspalma@gmail.com>
2024-11-17 19:35:59 -0300

    Append store to products

--- src/domains/product/parse-product-rows/index.ts
+++ src/domains/product/parse-product-rows/index.ts
@@ -2,15 +2,19 @@ import { NodePgDatabase } from "drizzle-orm/node-postgres";
 import { ProductFrontendRow } from "./types";
 import { IReviewAdapter, makePgReviewAdapter } from "../../../infra/database/review";
 import { Product } from "../../../database/schema/products";
+import { IGetStoreByUuid } from "../../store/get-store-by-id/interface";
+import { makeGetStoreByIdUseCase } from "../../store/get-store-by-id";
 
 export function makeParseProductRowsUseCase(dbConnection: NodePgDatabase) {
 	return new ParseProductRowsUseCase(
+		makeGetStoreByIdUseCase(dbConnection),
 		makePgReviewAdapter(dbConnection)
 	)
 }
 class ParseProductRowsUseCase {
 	constructor(
-		private readonly reviewAdapter: IReviewAdapter
+		private readonly getStoreByUuid: IGetStoreByUuid,
+		private readonly reviewAdapter: IReviewAdapter,
 	) { }
 	async execute(products: Product[], loggedUserUuid: string): Promise<ProductFrontendRow[]> {
 		const productIds = products.map(p => p.uuid)
@@ -24,6 +28,8 @@ class ParseProductRowsUseCase {
 
 		const productRows: ProductFrontendRow[] = []
 		for (let i = 0; i < products.length; i++) {
+			const store = await this.getStoreByUuid.execute(productRows[i].storeUuid, loggedUserUuid)
+
 			const product = products[i]
 			const average = averages.find(a => a.assetUuid === product.uuid)
 			const review = reviews.find(r => r.assetUuid === product.uuid)
@@ -36,7 +42,8 @@ class ParseProductRowsUseCase {
 				description: product.description,
 				createdAt: product.createdAt,
 				averageScore: average?.averageScore || null,
-				myReview: review || null
+				myReview: review || null,
+				store
 			})
 		}
 		return productRows
--- src/domains/product/parse-product-rows/types.ts
+++ src/domains/product/parse-product-rows/types.ts
@@ -1,7 +1,9 @@
 import { Product } from "../../../database/schema/products";
 import { Review } from "../../../database/schema/review";
+import { StoreWithLazyLoadedAssets } from "../../store/get-store-by-id/interface";
 
 export type ProductFrontendRow = Product & {
+	store: StoreWithLazyLoadedAssets;
 	averageScore: string | null;
 	myReview: Review | null;
 }
