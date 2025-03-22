485cc90a06fcb62a57aef6edd90afa307dd79ef3
Pedro Palma <pedrolucaspalma@gmail.com>
2025-03-22 20:36:01 -0300

    Add new stripe route for checkout-session creation

--- src/routes/stripe.ts
+++ src/routes/stripe.ts
@@ -1,35 +1,58 @@
 import express from "express";
 import { getEnvValues } from "../constants/EnvironmentVariables";
 import { makePgStripeEventAdapter } from "../infra/database/stripeEvent";
-import stripePkg from "stripe"
+import stripePkg from "stripe";
 import { StripeWebhookEventType } from "../database/schema/stripe-events";
 import { makePgAdminSubscriptionsAdapter } from "../infra/database/admin-subscription";
+import { authenticationMiddleware } from "../middlewares/auth/auth";
 
 const router = express.Router();
-const envValues = getEnvValues()
-const stripeEventAdapter = makePgStripeEventAdapter()
-const adminSubscriptionAdapter = makePgAdminSubscriptionsAdapter()
+const envValues = getEnvValues();
+const stripeEventAdapter = makePgStripeEventAdapter();
+const adminSubscriptionAdapter = makePgAdminSubscriptionsAdapter();
 
 // @ts-ignore
-const stripe = stripePkg(envValues.STRIPE_SECRET_KEY)
+const stripe = stripePkg(envValues.STRIPE_SECRET_KEY);
 
-router.post("/webhook", express.raw({ type: 'application/json' }), (req, res, next) => {
-    const signature = req.headers['stripe-signature'];
+router.post("/checkout-session", authenticationMiddleware(), async (req, res, next) => {
+    const { id } = req.user;
+
+    const session = await stripe.checkout.sessions
+        .create({
+            mode: "subscription",
+            payment_method_types: ["card"],
+            line_items: [{ price: "price_1QJvzcP7sqyH9Y3ciWQiGAPu", quantity: 1 }],
+            success_url: "https://www.google.com",
+            cancel_url: "https://www.google.com",
+            client_reference_id: id,
+        })
+        .catch((e: any) => {
+            res.status(500).send({
+                error: "internal server error while attempting to create checkout session",
+            });
+            next(e);
+        });
+
+    res.status(200).send({ id: session.id, url: session.url });
+});
+
+router.post("/webhook", express.raw({ type: "application/json" }), (req, res, next) => {
+    const signature = req.headers["stripe-signature"];
     const secret = envValues.STRIPE_WEBHOOK_SECRET_KEY;
 
     // @ts-ignore
     const event = stripe.webhooks.constructEvent(req.body, signature, secret);
 
-    handleEvent(event).catch(console.log)
+    handleEvent(event).catch(console.log);
 
-    res.status(200).send()
-})
+    res.status(200).send();
+});
 
 async function handleEvent(event: any) {
     const createdEvent = await stripeEventAdapter.create({
         event: event,
-        eventId: event?.id
-    })
+        eventId: event?.id,
+    });
 
     const userUuid = event?.data?.object?.metadata?.userUuid;
 
@@ -41,7 +64,7 @@ async function handleEvent(event: any) {
                 objectId: event?.data?.object?.id,
                 startsAt: new Date(event?.data?.object?.current_period_start * 1000),
                 expiresAt: new Date(event?.data?.object?.current_period_end * 1000),
-            })
+            });
             break;
         }
 
@@ -49,13 +72,11 @@ async function handleEvent(event: any) {
             const objectId = event?.data?.object?.id;
             await adminSubscriptionAdapter.updateByObjectId(objectId, {
                 cancelationEventUuid: createdEvent?.uuid,
-            })
+            });
             break;
         }
         default:
     }
-
 }
-
 
 export default router;
