acb14ca4a2d3e9ccc07d7193706b03644c400176
Pedro Palma <pedrolucaspalma@gmail.com>
2025-04-01 20:29:24 -0300

    fix seed

--- src/database/seeds/5-create-default-subscriptions.ts
+++ src/database/seeds/5-create-default-subscriptions.ts
@@ -5,7 +5,7 @@ import { StripeEvent } from "../schema/stripe-events";
 import { makePgStripeEventAdapter } from "../../infra/database/stripeEvent";
 import { makePgAdminSubscriptionsAdapter } from "../../infra/database/admin-subscription";
 
-const mockDate = new Date("1950-00-00")
+const mockDate = new Date("1950-01-01")
 const event: StripeEvent = {
     uuid: "5632505d-85de-41db-ac62-ef84cdfb7b64",
     createdAt: mockDate,
@@ -18,7 +18,7 @@ const subscriptions: AdminSubscription[] = [
         uuid: "d12ade59-f4d3-4e49-85fb-dca2e8487882",
         objectId: "id-qlqr-de-subscription",
         startsAt: mockDate,
-        expiresAt: new Date("3000-01--01"),
+        expiresAt: new Date("3000-01-01"),
         userUuid: TestDatabaseCommonValues.user1.uuid,
         createdAt: mockDate,
         creationEventUuid: event.uuid,
