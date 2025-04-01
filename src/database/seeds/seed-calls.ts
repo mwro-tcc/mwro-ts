bea6761383b6a7ae46565d1d8e85068bd3a929ed
Pedro Palma <pedrolucaspalma@gmail.com>
2025-03-31 22:01:14 -0300

    Call new seed

--- src/database/seeds/seed-calls.ts
+++ src/database/seeds/seed-calls.ts
@@ -3,10 +3,12 @@ import { populate as createUsers } from "./1-create-default-users";
 import { populate as createCommunities } from "./2-create-default-communities";
 import { populate as createStores } from "./3-create-default-stores";
 import { populate as createProducts } from "./4-create-default-products";
+import { populate as createSubscriptions } from "./5-create-default-subscriptions";
 
 export default async function populate(db: NodePgDatabase) {
     await createUsers(db);
     await createCommunities(db);
     await createStores(db);
     await createProducts(db);
+    await createSubscriptions(db)
 }
