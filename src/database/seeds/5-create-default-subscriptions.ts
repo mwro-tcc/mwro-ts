ab48d46825094a54a9fb37b64aedaf96c96a8d84
Pedro Palma <pedrolucaspalma@gmail.com>
2025-03-31 21:59:57 -0300

    Add new seed

---
+++ src/database/seeds/5-create-default-subscriptions.ts
@@ -0,0 +1,35 @@
+import { NodePgDatabase } from "drizzle-orm/node-postgres";
+import { TestDatabaseCommonValues } from "../../constants/TestDatabaseSeedValues";
+import { AdminSubscription } from "../schema/admin-subscriptions";
+import { StripeEvent } from "../schema/stripe-events";
+import { makePgStripeEventAdapter } from "../../infra/database/stripeEvent";
+import { makePgAdminSubscriptionsAdapter } from "../../infra/database/admin-subscription";
+
+const mockDate = new Date("1950-00-00")
+const event: StripeEvent = {
+    uuid: "5632505d-85de-41db-ac62-ef84cdfb7b64",
+    createdAt: mockDate,
+    event: { foo: "bar" },
+    eventId: "qlqr-id-de-evento-stripe"
+
+}
+const subscriptions: AdminSubscription[] = [
+    {
+        uuid: "d12ade59-f4d3-4e49-85fb-dca2e8487882",
+        objectId: "id-qlqr-de-subscription",
+        startsAt: mockDate,
+        expiresAt: new Date("3000-00--00"),
+        userUuid: TestDatabaseCommonValues.user1.uuid,
+        createdAt: mockDate,
+        creationEventUuid: event.uuid,
+        cancelationEventUuid: null
+    },
+];
+
+export async function populate(db: NodePgDatabase) {
+    const stripeEventAdapter = makePgStripeEventAdapter(db);
+    await stripeEventAdapter.create(event)
+
+    const adminSubscriptionAdapter = makePgAdminSubscriptionsAdapter(db)
+    await adminSubscriptionAdapter.bulkCreate(subscriptions)
+}
