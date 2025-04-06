2b1003cd1ca88322188a66f9ecdaf15340ce0efb
Pedro Palma <pedrolucaspalma@gmail.com>
2025-04-06 20:09:02 -0300

    upd

--- src/validations/UpdateCommunity.ts
+++ src/validations/UpdateCommunity.ts
@@ -6,5 +6,7 @@ export const updateCommunitySchema = z.object({
         name: z.string().trim().optional(),
         isPrivate: z.boolean().optional(),
         description: z.string().trim().optional(),
+        latitude: z.number().optional(),
+        longitude: z.number().optional(),
     }),
 });
